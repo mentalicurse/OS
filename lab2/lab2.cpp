bd3b22155b30f72862cb938ade478606de997f64
mentalicurse <121573729+mentalicurse@users.noreply.github.com>
2024-01-09 20:19:32 +0700

    Update lab2.cpp

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 #include <signal.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/select.h>
 
-using namespace std;
-
-#define PORT 8080
-#define MAX_CLIENTS 10
+typedef struct {
+    int connfd;
+    struct sockaddr_in addr;
+} client_t;
 
 volatile sig_atomic_t wasSigHup = 0;
 
@@ -18,119 +18,117 @@ void sigHupHandler(int r) {
     wasSigHup = 1;
 }
 
-int main() {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    fd_set readfds;
-    int max_sd, sd, activity;
-    int clients[MAX_CLIENTS] = {0};
-    int max_clients = MAX_CLIENTS;
-    int client_socket, i;
+int createServer(int port) {
+    int serverSocket;
+    struct sockaddr_in serverAddress;
 
-    // Создание сокета
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Регистрация обработчика сигнала
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(port);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) != 0) {
+        perror("Socket bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverSocket, 3) != 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return serverSocket;
+}
+
+int main() {
+    int serverSocket = createServer(8080);
+    puts("Server is listening...");
+
+    client_t clients[3];
+    int activeClients = 0;
+    fd_set readfds;
+    int maxFd, activity, i;
+    char buffer[1024] = {0};
+
     struct sigaction sa;
     sigaction(SIGHUP, NULL, &sa);
     sa.sa_handler = sigHupHandler;
     sa.sa_flags |= SA_RESTART;
     sigaction(SIGHUP, &sa, NULL);
 
-    // Блокирование сигнала
-    sigset_t blockedMask;
+    sigset_t blockedMask, origMask;
     sigemptyset(&blockedMask);
     sigaddset(&blockedMask, SIGHUP);
-    sigprocmask(SIG_BLOCK, &blockedMask, NULL);
+    sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
 
-    // Установка опций сокета
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Привязка сокета к адресу и порту
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Прослушивание сокета
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    // Основной цикл приложения
     while (1) {
-        // Подготовка списка файловых дескрипторов
-        FD_ZERO(&readfds);
-        FD_SET(server_fd, &readfds);
-        max_sd = server_fd;
-        for (i = 0; i < max_clients; i++) {
-            sd = clients[i];
-            if (sd > 0) {
-                FD_SET(sd, &readfds);
-            }
-            if (sd > max_sd) {
-                max_sd = sd;
-            }
-        }
-
-        // Вызов функции pselect()
-        activity = pselect(max_sd + 1, &readfds, NULL, NULL, NULL, &blockedMask);
-
-        if (activity < 0 && errno != EINTR) {
-            perror("pselect error");
-        }
-
-        // Обработка произошедшего события
-        if (FD_ISSET(server_fd, &readfds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-            cout << "New connection: " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << endl;
-
-            // Оставляем только одно соединение
-            for (i = 0; i < max_clients; i++) {
-                if (clients[i] == 0) {
-                    clients[i] = new_socket;
-                    break;
-                }
-                else {
-                    close(new_socket);
-                }
-            }
-        }
-
-        for (i = 0; i < max_clients; i++) {
-            client_socket = clients[i];
-            if (FD_ISSET(client_socket, &readfds)) {
-                if ((valread = read(client_socket, buffer, 1024)) == 0) {
-                    getpeername(client_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                    cout << "Host disconnected: " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << endl;
-                    close(client_socket);
-                    clients[i] = 0;
-                }
-                else {
-                    cout << "Data received: " << valread << endl;
-                }
-            }
-        }
-
         if (wasSigHup) {
-            cout << "SIGHUP received" << endl;
             wasSigHup = 0;
+            puts("Clients:");
+            for (int i = 0; i < activeClients; i++) {
+                printf("[%s:%d] ", inet_ntoa(clients[i].addr.sin_addr), htons(clients[i].addr.sin_port));
+            }
+            puts("\n");
+        }
+
+        FD_ZERO(&readfds);
+        FD_SET(serverSocket, &readfds);
+        maxFd = serverSocket;
+
+        for (int i = 0; i < activeClients; i++) {
+            FD_SET(clients[i].connfd, &readfds);
+            if (clients[i].connfd > maxFd) {
+                maxFd = clients[i].connfd;
+            }
+        }
+
+        activity = pselect(maxFd + 1, &readfds, NULL, NULL, NULL, &origMask);
+
+        if (activity == -1) {
+            if (errno == EINTR && wasSigHup) {
+                printf("Received SIGHUP signal\n");
+                wasSigHup = 0;
+            } else {
+                perror("pselect failed");
+                return -1;
+            }
+        }
+
+        if (FD_ISSET(serverSocket, &readfds) && activeClients < 3) {
+            client_t *client = &clients[activeClients];
+            int len = sizeof(client->addr);
+            int connfd = accept(serverSocket, (struct sockaddr*)&client->addr, &len);
+            if (connfd >= 0) {
+                client->connfd = connfd;
+                printf("[%s:%d] Connected!\n", inet_ntoa(client->addr.sin_addr), htons(client->addr.sin_port));
+                activeClients++;
+            } else {
+                perror("Connection error");
+            }
+        }
+
+        for (i = 0; i < activeClients; i++) {
+            client_t *client = &clients[i];
+            if (FD_ISSET(client->connfd, &readfds)) {
+                int readLen = read(client->connfd, buffer, sizeof(buffer) - 1);
+                if (readLen > 0) {
+                    buffer[readLen - 1] = 0;
+                    printf("[%s:%d] %s\n", inet_ntoa(client->addr.sin_addr), htons(client->addr.sin_port), buffer);
+                } else {
+                    close(client->connfd);
+                    printf("[%s:%d]", inet_ntoa(client->addr.sin_addr), htons(client->addr.sin_port));
+                    puts(" Соединение закрыто\n");
+                    clients[i] = clients[activeClients - 1];
+                    activeClients--;
+                    i--;
+                }
+            }
         }
     }
 
